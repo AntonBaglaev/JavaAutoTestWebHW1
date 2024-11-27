284f33c882b95ea45871a357a80cbaaf1a3763f7
Anton Baglaev <abaglaev@yandex.ru>
2024-11-27 17:50:53 +0300

    add homework1

--- src/test/java/org/example/PerfectGeekBrainsTests.java
+++ src/test/java/org/example/PerfectGeekBrainsTests.java
@@ -1,16 +1,14 @@
 package org.example;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -36,7 +34,7 @@ public class PerfectGeekBrainsTests {
     }
 
     @Test
-    public void testStandGeekBrains() {
+    public void testStandGeekBrains() throws IOException {
         driver.get("https://test-stand.gb.ru/login");
         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("form#login input[type='text']"))).sendKeys(USERNAME);
         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("form#login input[type='password']"))).sendKeys(PASSWORD);
@@ -47,6 +45,33 @@ public class PerfectGeekBrainsTests {
 
         WebElement usernameLink = wait.until(ExpectedConditions.visibilityOfElementLocated(By.partialLinkText(USERNAME)));
         assertEquals(String.format("Hello, %s", USERNAME), usernameLink.getText().replace("\n", " ").trim());
+        //getScreen();
+    }
+
+    @Test
+    void groupAddingTest() throws  IOException {
+        testStandGeekBrains();
+        String groupName = "New Study Group " + System.currentTimeMillis();
+        WebElement createGroup = wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//*[@id='create-btn']")));
+        createGroup.click();
+        By fieldGroupName = By.xpath("//*[@type='text']");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(fieldGroupName)).sendKeys(groupName);
+        WebElement buttonSaveGroup = wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.cssSelector("form div.submit button")));
+        buttonSaveGroup.click();
+
+        String tableTitleXpath = "//td[contains(text(), '%s')]";
+        WebElement expectedTitle = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath(String.format(tableTitleXpath, groupName))));
+        Assertions.assertTrue(expectedTitle.isDisplayed());
+        getScreen();
+    }
+
+    private void getScreen() throws IOException {
+        byte[] screenshotBytes = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+        Files.write(Path.of(
+                "src/main/resources/screenshot_" + System.currentTimeMillis() + ".png"), screenshotBytes);
     }
 
     @AfterEach
